1fc1172b7d9293a92d963ee4bda0c2059827a892
SamuelCarriles <samuelcarrilesbanos@gmail.com>
2025-03-26 21:06:09 -0400

    url-encoding function add

--- src/fhir_search/uri_parser.clj
+++ src/fhir_search/uri_parser.clj
@@ -3,27 +3,45 @@
   (:require [clojure.walk :refer [postwalk]])
   (:import [java.net URI]))
 
-(defn gen-values [query]
+
+
+(defn url-encoding [url]
+  (let [uri (URI. url)
+        scheme (.getScheme uri)
+        authority (.getAuthority uri)
+        path (.getPath uri)
+        query (.getQuery uri)
+        fragment (.getFragment uri)]
+    (when uri
+      (str (when scheme (str scheme "://"))
+           authority path
+           (when query (str "?" query))
+           (when fragment (str "#" fragment))))))
+
+(defn values [query]
   (->> 
    (str/split query #"&")
    (reduce (fn [result element]
-             (let [first-group (first (str/split element #"=")) second-group (second (str/split element #"="))]
+             (let [group (str/split element #"=") 
+                   first-group (first group) 
+                   second-group (second group)]
                (conj result {:name (first (str/split first-group #":"))
-                             :modifiers (when (not (nil? (second (str/split first-group #":")))) 
-                                          (keyword (str "fhir.search.modifier/" (second (str/split first-group #":")))))
+                             :modifiers (when (second (str/split first-group #":")) 
+                                          (keyword "fhir.search.modifier" (second (str/split first-group #":"))))
                              :value second-group}))) [])))
 
-(defn get-path [string]
-  (let [path (remove empty? (-> (.getPath (URI. string)) (str/split #"/"))) structure {:type (last path)}]
+(defn path [string]
+  (let [path (remove empty? (-> (.getPath (URI. string)) (str/split #"/"))) 
+        structure {:type (last path)}]
     (cond-> structure
       (= 3 (count path)) (assoc :compartment {:type (first path) :id (second path)})
       (= 2 (count path)) (assoc :component {:id (first path)}))))
 
-(defn get-params [string]
-  (let [query (.getQuery (URI. string)) structure {:params nil}]
-    (when (not (nil? query))
-      (assoc structure :params {:join :fhir.search.join/and
-                                :values (gen-values query)}))))
+(defn params [string]
+  (let [query (.getQuery (URI. string))]
+    (when query
+      (hash-map :params {:join :fhir.search.join/and
+                                :values (values query)}))))
 
 (defn clean [m]
   (postwalk (fn [v]
@@ -34,30 +52,30 @@
                 (seq? v) (remove nil? v)
                 :else v)) m))
 
-(defn uri-parser [url] (postwalk clean (into (get-path url) (get-params url))))
+(defn uri-parser [url] (postwalk clean (into (path url) (params url))))
 
 
 (comment
+  (url-encoding "/Observation?code=http%3A%2F%2Floinc.org%7C8867-4&value-quantity=lt60%2Cgt100") 
+  ;; "/Observation?code=http://loinc.org|8867-4&value-quantity=lt60,gt100"
 
-  (get-path "/Condition")
+  (path "/Condition")
   ;;  {:type "Condition"}
 
-  (get-path "/Patient/p123/Condition")
+  (path "/Patient/p123/Condition")
   ;;  {:type "Condition" 
   ;;   :compartment {:type "Patient"
   ;;                 :id "p123"}}
 
-  (get-params "/Patient/p123/Condition?code:in=http%3A%2F%2Fhspc.org%2FValueSet%2Facute-concerns")
+  (params "/Patient/p123/Condition?code:in=http%3A%2F%2Fhspc.org%2FValueSet%2Facute-concerns")
   ;;  {:params {:join :fhir.search.join/and
   ;;             :values [{:name "code"
   ;;                       :modifiers :fhir.search.modifier/in
-  ;;                       :value "http://hspc.org/ValueSet/acute-concerns"}]}}
+  ;;                       :value "http://hspc.org/ValueSet/acute-concerns"}]}} 
 
-  
-  
   (uri-parser "/Patient/p123/Condition")
   ;;  {:type "Condition", :compartment {:type "Patient", :id "p123"}}
-  
+
   (uri-parser "/Patient/p123/Condition?code:in=http%3A%2F%2Fhspc.org%2FValueSet%2Facute-concerns")
   ;;  {:type "Condition",
   ;;    :compartment {:type "Patient"
