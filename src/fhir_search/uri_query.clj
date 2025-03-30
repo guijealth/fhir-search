e00881b686707d5a1627700788b865870264defe
SamuelCarriles <samuelcarrilesbanos@gmail.com>
2025-03-30 15:41:57 -0400

    prefix fn and uri-parse update

---
+++ src/fhir_search/uri_query.clj
@@ -0,0 +1,100 @@
+(ns fhir-search.uri-query
+  (:require [clojure.string :as str]
+            [clojure.walk :refer [postwalk]]
+            [fhir-search.complex :refer [clean]])
+  (:import [java.net URI]))
+
+(defn prefix-finder
+  [element]
+  (when-let [[_ prefix value] (first (re-seq #"(eq|ne|gt|lt|ge|le|sa|eb|ap)?(\d.*)" element))]
+    [prefix value]))
+
+(defn values [query]
+  (->>
+   (str/split query #"&")
+   (reduce (fn [result element]
+             (let [group (str/split element #"=")
+                   first-group (first group)
+                   second-group (second group)
+                   pf-group (prefix-finder second-group)]
+               (if (re-find #"," second-group)
+                 (conj result (->>
+                               (reduce (fn [value item]
+                                         (let [prefix-group (prefix-finder item)]
+                                           (conj value {:modifiers (when (second (str/split first-group #":"))
+                                                                     (keyword "fhir.search.modifier" (second (str/split first-group #":"))))
+                                                        :prefix (when (first prefix-group) (keyword "fhir.search.prefix" (first prefix-group)))
+                                                        :value (if (first prefix-group)
+                                                                 (second prefix-group)
+                                                                 item)})))
+                                       [] (str/split second-group #","))
+                               (assoc {:join :fhir.search.join/or
+                                       :name (first (str/split first-group #":"))} :values)))
+
+                 (conj result {:name (first (str/split first-group #":"))
+                               :modifiers (when (second (str/split first-group #":"))
+                                            (keyword "fhir.search.modifier" (second (str/split first-group #":"))))
+                               :prefix (when (first pf-group) (keyword "fhir.search.prefix" (first pf-group)))
+                               :value (if (first pf-group)
+                                        (second pf-group)
+                                        second-group)})))) [])))
+
+(defn path [string]
+  (let [path (remove empty? (str/split string #"/"))
+        structure {:type (last path)}]
+    (cond-> structure
+      (= 3 (count path)) (assoc :compartment {:type (first path) :id (second path)})
+      (= 2 (count path)) (assoc :component {:id (first path)}))))
+
+(defn params [query] 
+    (when query
+      (hash-map :params {:join :fhir.search.join/and
+                         :values (values query)})))
+
+(defn uri-parse [url] 
+    (let [uri (URI. url)]
+      (postwalk clean (into (path (.getPath uri)) (params (.getQuery uri))))))
+
+
+(comment
+  (uri-parse "/Patient/p123/Condition")
+  ;;  {:type "Condition", :compartment {:type "Patient", :id "p123"}}
+
+  (uri-parse "/Patient/p123/Condition?code:in=http%3A%2F%2Fhspc.org%2FValueSet%2Facute-concerns")
+  ;;  {:type "Condition",
+  ;;    :compartment {:type "Patient"
+  ;;                  :id "p123"}
+  ;;    :params {:join :fhir.search.join/and 
+  ;;             :values [{:name "code"
+  ;;                    :modifiers :fhir.search.modifier/in
+  ;;                    :value "http://hspc.org/ValueSet/acute-concerns"}]}}
+
+  (uri-parse "/Patient?given:exact=GivenA,GivenB")
+  ;; {:type "Patient",
+  ;;  :params
+  ;;  {:join :fhir.search.join/and
+  ;;   :values
+  ;;   [{:join :fhir.search.join/or
+  ;;     :name "given"
+  ;;     :values
+  ;;     [{:modifiers :fhir.search.modifier/exact
+  ;;       :value "GivenA"}
+  ;;      {:modifiers :fhir.search.modifier/exact
+  ;;       :value "GivenB"}]}]}}
+
+  (uri-parse "/Observation?code:in=http%3A%2F%2Floinc.org%7C8867-4&value-quantity=lt60%2Cgt100")
+  ;; {:type "Observation"
+  ;;  :params
+  ;;  {:join :fhir.search.join/and
+  ;;   :values
+  ;;   [{:name "code"
+  ;;     :modifiers :fhir.search.modifier/in
+  ;;     :value "http://loinc.org|8867-4"}
+  ;;    {:join :fhir.search.join/or
+  ;;     :name "value-quantity"
+  ;;     :values [{:prefix :fhir.search.prefix/lt
+  ;;               :value "60"} 
+  ;;             {:prefix :fhir.search.prefix/gt
+  ;;              :value "100"}]}]}} 
+  )
+
