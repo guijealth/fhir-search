ff2284a9967a03061cd3a991278bfe2617245777
SamuelCarriles <samuelcarrilesbanos@gmail.com>
2025-03-31 19:31:07 -0400

    composite parameters supported

--- src/fhir_search/uri_query.clj
+++ src/fhir_search/uri_query.clj
@@ -6,7 +6,7 @@
 
 (defn prefix-finder
   [element]
-  (when-let [[_ prefix value] (first (re-seq #"(eq|ne|gt|lt|ge|le|sa|eb|ap)?(\d.*)" element))]
+  (when-let [[_ prefix value] (first (re-seq #"(eq|ne|gt|lt|ge|le|sa|eb|ap)(\d.*)" element))]
     [prefix value]))
 
 (defn param-name [string]
@@ -18,41 +18,57 @@
    (second (str/split string #"="))
    (str/split #",")))
 
+;;Composite Parameters section
+
+(defn composite-param? [string]
+  (when (re-find #"\$" string) true))
+
+(defn comp-group [string]
+  (str/split string #"\$"))
+
 (defn values [query]
   (->> (str/split query #"&")
        (reduce (fn [result element]
                  (let [param-name (param-name element)
                        param-values (param-value element)]
-                   (prn (count param-values))
                    (if (> (count param-values) 1)
                      (conj result
                            (->> (reduce (fn [values item]
-                                          (let [pf-group (prefix-finder item)]
-                                            (conj values {:modifier (when-let [modif (second param-name)]
+                                          (let [pf-group (prefix-finder item) comp-group (comp-group item)]
+                                            (conj values {:name (when (composite-param? item) (first comp-group))
+                                                          ;;
+                                                          :modifier (when-let [modif (second param-name)]
                                                                       (keyword "fhir.search.modifier" modif))
                                                           ;;
                                                           :prefix (when-let [prefix (first pf-group)]
                                                                     (keyword "fhir.search.prefix" prefix))
                                                           ;;
-                                                          :value (if (first pf-group)
-                                                                   (second pf-group)
-                                                                   item)})))
+                                                          :value (cond
+                                                                   (first pf-group) (second pf-group)
+                                                                   (composite-param? item) (second comp-group)
+                                                                   :else item)})))
                                         [] param-values)
                                 (assoc {:join :fhir.search.join/or
-                                        :name (first param-name)} :values)))
+                                        :name (first param-name)
+                                        :composite (when (some composite-param? param-values) true)} :values)))
                      ;;
-                     (conj result (let [pf-group (prefix-finder (first param-values))]
+                     (conj result (let [pf-group (prefix-finder (first param-values)) comp-group (comp-group (first param-values))]
                                     {:name (first param-name)
                                      ;;
                                      :modifiers (when-let [modif (second param-name)]
                                                   (keyword "fhir.search.modifier" modif))
                                      ;;
+                                     :composite (when (composite-param? (first param-values)) true)
+                                     ;;
                                      :prefix (when-let [prefix (first pf-group)]
                                                (keyword "fhir.search.prefix" prefix))
                                      ;;
-                                     :value (if (first pf-group)
-                                              (second pf-group)
-                                              (first param-values))}))))) [])))
+                                     ;;
+                                     :value (cond
+                                              (first pf-group) (second pf-group)
+                                              (composite-param? (first param-values)) [{:name (first comp-group)
+                                                                                        :value (second comp-group)}]
+                                              :else (first param-values))}))))) [])))
 
 (defn path [string]
   (let [path (remove empty? (str/split string #"/"))
@@ -110,5 +126,27 @@
   ;;               :value "60"} 
   ;;             {:prefix :fhir.search.prefix/gt
   ;;              :value "100"}]}]}} 
+  (uri-parse "/DiagnosticReport?result=http://loinc.org%7C2823-3$gt5.4%7Chttp://unitsofmeasure.org%7Cmmol/L")
+  ;; {:type "DiagnosticReport"
+  ;;  :params
+  ;;  {:join :fhir.search.join/and
+  ;;   :values [{:name "result"
+  ;;             :composite true
+  ;;             :prefix :fhir.search.prefix/gt
+  ;;             :value "5.4|http://unitsofmeasure.org|mmol/L"}]}}
+  (uri-parse "/Observation?code-value-quantity=code$loinc%7C12907-2,value$ge150%7Chttp://unitsofmeasure.org%7Cmmol/L&based-on=ServiceRequest/f8d0ee15-43dc-4090-a2d5-379d247672eb")
+  ;; {:type "Observation",
+  ;;  :params
+  ;;  {:join :fhir.search.join/and
+  ;;   :values [{:join :fhir.search.join/or
+  ;;             :name "code-value-quantity"
+  ;;             :composite true
+  ;;             :values [{:name "code"
+  ;;                       :value "loinc|12907-2"}
+  ;;                      {:name "value"
+  ;;                       :prefix :fhir.search.prefix/ge
+  ;;                       :value "150|http://unitsofmeasure.org|mmol/L"}]}
+  ;;            {:name "based-on"
+  ;;             :value "ServiceRequest/f8d0ee15-43dc-4090-a2d5-379d247672eb"}]}}
   )
 
