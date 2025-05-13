a349950ac1e27cec630405e555b251a76a9e4994
SamuelCarriles <samuelcarrilesbanos@gmail.com>
2025-05-13 17:42:33 -0400

    reverse chained supported

--- src/fhir_search/core.clj
+++ src/fhir_search/core.clj
@@ -27,15 +27,17 @@
                   :value (or v2 v1 v)
                   :prefix prefix})))))
 
-(defn parse-chain [param]
-  (let [{:keys [name modifier value params composite]} param]
-    (if-let [chain (seq (str/split name #"\."))]
-      (->> chain
-           (map (fn [part]
-                  (let [[name type] (str/split part #":")]
-                    {:name name
-                     :type type})))
-           (reverse)
+(defn build-chain "Construye una estructura anidada a partir de segmentos pre-parseados.
+   - segments: secuencia de mapas que representan eslabones de la cadena.
+   - keys-to-first: mapa con :modifier, :value, :params, :composite para el primer eslabón.
+   - addit-keys: keys adicionales mapa con keys adicionales como :chained, :reverse."
+  
+  [segments keys-to-first addit-keys]
+  (when (seq segments)
+    (let [seg (reverse segments)
+          {:keys [modifier value params composite]} keys-to-first
+          {:keys [chained reverse]} addit-keys]
+      (->> seg
            (map-indexed (fn [idx item]
                           (cond-> item
                             (zero? idx) (assoc :modifier modifier
@@ -46,24 +48,63 @@
            (reduce (fn [acc curr]
                      (assoc curr
                             :params [acc]
-                            :chained true))))
+                            :reverse reverse
+                            :chained chained)))))))
+
+(defn parse-has [param]
+  (let [{:keys [name modifier value params composite]} param
+        segments (->> (str/split name #"\.")
+                      (map #(re-seq #"(?<=:)[^:._]+" %))
+                      (flatten)
+                      (partition-all 2)
+                      (reduce (fn [acc curr]
+                                (conj acc (let [[part1 part2] curr]
+                                            (case (count curr)
+                                              1 {:name part1}
+                                              2 {:name part2
+                                                 :type part1}))))
+                              []))
+        keys-to-first {:modifier modifier
+                       :value value
+                       :params params
+                       :composite composite}
+        addit-keys {:reverse true
+                    :chained (when (re-find #"\." name) true)}]
+    (build-chain segments keys-to-first addit-keys)))
+
+(defn parse-chain [param]
+  (let [{:keys [name modifier value params composite]} param]
+    (if-let [chain (seq (str/split name #"\."))]
+      (let [segments (->> chain
+                          (map (fn [part]
+                                 (let [[name type] (str/split part #":")]
+                                   {:name name
+                                    :type type}))))
+            keys-to-first {:modifier modifier
+                           :value value
+                           :params params
+                           :composite composite}
+            addit-keys {:chained true}]
+        (build-chain segments keys-to-first addit-keys))
       param)))
 
 (defn parse-query [query]
   (->> (str/split query #"&")
-       (map (fn [param]
-              (let [[key value] (str/split param #"=")
-                    [_ name mod] (re-find modifier-pattern key)
-                    modifier (when mod
-                               (keyword "fhir.search.modier" mod))
-                    params (parse-value value modifier)
-                    params-c (count params)]
-                (cond-> {:name (or name key)}
-                  (= 1 params-c) (assoc :modifier modifier
-                                        :value value)
-                  (> params-c 1) (assoc :params params)
-                  (seq (filter :name params)) (assoc :composite true)))))
-       (mapv parse-chain)))
+                        (map (fn [param]
+                               (let [[key value] (str/split param #"=")
+                                     [_ name mod] (re-find modifier-pattern key)
+                                     modifier (when mod
+                                                (keyword "fhir.search.modier" mod))
+                                     params (parse-value value modifier)
+                                     params-c (count params)]
+                                 (cond-> {:name (or name key)}
+                                   (= 1 params-c) (assoc :modifier modifier
+                                                         :value value)
+                                   (> params-c 1) (assoc :params params)
+                                   (seq (filter :name params)) (assoc :composite true)))))
+    (mapv #(if (re-find #"_has:" (:name %))
+             (parse-has %)
+             (parse-chain %)))))
 
 (defn parse [fhir-query]
   (let [url ^URI (URI. fhir-query)]
@@ -74,7 +115,11 @@
 
 (comment
 
-  (time
-   (parse "/Patient?general-practitioner:PractitionerRole.practitioner:Practitioner.name:contains=John&organization=Organization/909823472760"))
+  
+   (parse "/Patient?general-practitioner:PractitionerRole.practitioner:Practitioner.name:contains=John&organization=Organization/909823472760")
+  (parse-query "_has:Observation:patient._has:AuditEvent:entity:agent=MyUserId")
+  (parse "/Patient?_has:Observation:patient._has:AuditEvent:entity:agent=MyUserId&general-practitioner:PractitionerRole.practitioner:Practitioner.name:contains=John&organization=Organization/909823472760")
+  
+
 
   :.)
